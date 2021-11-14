f393d4ae6e373ae2ede1c91bb916d7edfa5801a6
Scott Nason <scottmnason@gmail.com>
2021-11-14 14:28:47 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(oldArray, eventName) {
+    const newArray = [];
+    for (let i = 0; i < oldArray.length; i++) {
+        newArray[i] = `Thank you, ${oldArray[i]}, for the wonderful ${eventName} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
